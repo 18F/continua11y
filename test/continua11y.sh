94b3fb5d6be2a7bf7dde7bb3a6836d5689d04dcf
stvnrlly <stevenmreilly+github@gmail.com>
2015-06-28 17:15:45 -0400

    dollar signs

--- test/continua11y.sh
+++ test/continua11y.sh
@@ -26,8 +26,8 @@ else
     npm install -g json
 fi
 
-echo TRAVIS_PULL_REQUEST
-echo TRAVIS_COMMIT_RANGE
+echo $TRAVIS_PULL_REQUEST
+echo $TRAVIS_COMMIT_RANGE
 
 TRAVIS_COMMIT_MSG="$(git log --format=%B --no-merges -n 1)"
 

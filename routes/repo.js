7c73572f4c71c4826d49fa3bd00cc933b81e353d
stvnrlly <stevenmreilly+github@gmail.com>
2015-10-26 22:39:56 -0400

    switch over to all glyphicons to avoid weird wget behavior

--- routes/repo.js
+++ routes/repo.js
@@ -10,36 +10,36 @@ exports.get = function (req, res){
     }).then(function (repo) {
         try {
             var defaultBranch = repo.defaultBranch;
+                models.Commit.findAll({
+                where: {
+                    repo: repo.repo
+                },
+                order: [['updatedAt', 'DESC']]
+            }).then(function (commits) {
+                var branches = [];
+                async.series([
+                    function (callback) {
+                        for (var i=0; i<commits.length; i++) {
+                            if (commits[i].latest === true) {
+                                branches.push(commits[i].branch);
+                            }
+                        }
+                        callback(null);
+                    },
+                    function (callback) {
+                        res.render('repo', {
+                            results: commits, 
+                            repo: req.params.account + '/' + req.params.repo, 
+                            branches: branches, 
+                            default_branch: defaultBranch});
+                        callback(null);
+                    }
+                ]);
+            });
         } catch (e) {
             // entering incorrect owner/repo urls (or urls that looks like that)
             // won't get caught by the middleware, so this catches them
             res.render('404');
         }
-        models.Commit.findAll({
-            where: {
-                repo: repo.repo
-            },
-            order: [['updatedAt', 'DESC']]
-        }).then(function (commits) {
-            var branches = [];
-            async.series([
-                function (callback) {
-                    for (var i=0; i<commits.length; i++) {
-                        if (commits[i].latest === true) {
-                            branches.push(commits[i].branch);
-                        }
-                    }
-                    callback(null);
-                },
-                function (callback) {
-                    res.render('repo', {
-                        results: commits, 
-                        repo: req.params.account + '/' + req.params.repo, 
-                        branches: branches, 
-                        default_branch: defaultBranch});
-                    callback(null);
-                }
-            ]);
-        });
     });
 };

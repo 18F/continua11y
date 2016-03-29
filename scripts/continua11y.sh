567dc0005e7d17459b2c8feb35fa09e4fd4c4429
stvnrlly <steven.reilly@gsa.gov>
2016-03-29 15:50:22 -0400

    avoid nohup

--- scripts/continua11y.sh
+++ scripts/continua11y.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
 npm install -g pa11y-crawl
-nohup npm start >/dev/null 2>&1 &
-cat nohup.out
-pa11y-crawl --ci http://localhost:3000
+npm start >/dev/null 2>&1 &
+pa11y-crawl --ci http://localhost:300
